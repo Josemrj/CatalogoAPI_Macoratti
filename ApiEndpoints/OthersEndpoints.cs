942d02eb7e46c8b52df2dda3d59873d6c27a0d06
José Eduardo Jesus <jose.eduardo@supermercadosbh.com.br>
2023-08-21 10:26:59 -0300

    Create static methods for only class for endpoints products and endpoint method for 'get JWT'.

---
+++ ApiEndpoints/OthersEndpoints.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace CatalogoAPI.ApiEndpoints
+{
+    public static class OthersEndpoints
+    {
+        public static void GetStringForJWT(this WebApplication app)
+        {
+            app.MapGet("/generate-kay", async () =>
+            {
+                string getString = "";
+                GenerateKay();
+
+                ActionResult<string> GenerateKay()
+                {
+                    int keyLength = 32; // Tamanho da chave em bytes (256 bits)
+                    byte[] keyBytes = new byte[keyLength];
+                    using (var rng = new RNGCryptoServiceProvider())
+                    {
+                        rng.GetBytes(keyBytes);
+                    }
+                    string jwtSecretKey = Convert.ToBase64String(keyBytes);
+                    getString = jwtSecretKey;
+
+                    return jwtSecretKey;
+                }
+
+                return Results.Ok(getString);
+            });
+        }
+    }
+}
---
+++ ApiEndpoints/ProdutosEndpoints.cs
@@ -0,0 +1,82 @@
+﻿using CatalogoAPI.Context;
+using CatalogoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoAPI.ApiEndpoints
+{
+    public static class ProdutosEndpoints
+    {
+        public static void MapProdutosEndpoints(this WebApplication app)
+        {
+            app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync() is null ? Results.NotFound("REC. NAO ENCONTRADO") : Results.Ok(await db.Produtos.ToListAsync())).WithTags("Produtos").RequireAuthorization();
+
+            app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db) =>
+            {
+                var products = await db.Produtos.FindAsync(id);
+
+                if (products.ProdutoId != id)
+                    return Results.BadRequest("REQUISICAO INVALIDA");
+
+                if (products == null)
+                    return Results.NotFound("RECURSO NAO ENCONTRADO");
+
+                return Results.Ok(products);
+            });
+
+            app.MapPost("/produtos", async (Produto produto, AppDbContext db) =>
+            {
+                try
+                {
+                    if (produto == null)
+                        return Results.NotFound("RECURSO NAO ENCONTRADO!");
+
+                    await db.AddAsync(produto);
+
+                    db.SaveChanges();
+
+                    return Results.Created($"/produtos/{produto.ProdutoId}:int", produto);
+                }
+                catch (Exception)
+                {
+                    return Results.StatusCode(StatusCodes.Status500InternalServerError);
+                }
+            });
+
+            app.MapPut("/produtos/{id:int}", async (int id, Produto inputProduto, AppDbContext db) =>
+            {
+                if (inputProduto == null)
+                    return Results.NotFound();
+
+                var produto = await db.Produtos.FindAsync(id);
+
+                if (inputProduto.ProdutoId != id)
+                    return Results.BadRequest("REQUISICAO INVALIDA");
+
+                produto.Nome = inputProduto.Nome;
+                produto.Descricao = inputProduto.Descricao;
+                produto.Preco = inputProduto.Preco;
+                produto.Imagem = inputProduto.Imagem;
+                produto.DataCompra = inputProduto.DataCompra;
+                produto.Estoque = inputProduto.Estoque;
+
+                await db.SaveChangesAsync();
+
+                return Results.Accepted($"/produtos/{produto.ProdutoId}:int", produto);
+            });
+
+            app.MapDelete("/produtos/{id:int}", async (int id, AppDbContext db) =>
+            {
+                var produtos = await db.Produtos.FindAsync(id);
+
+                if (produtos == null)
+                    return Results.NotFound();
+
+                db.Remove(produtos);
+
+                await db.SaveChangesAsync();
+
+                return Results.NoContent();
+            });
+        }
+    }
+}
